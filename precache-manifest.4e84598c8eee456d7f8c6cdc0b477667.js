b1e88d6bc0ff8f4014723071667f9e3d1bcd77ae
elefcodes <panteliselef@outlook.com>
2019-05-30 15:46:08 +0300

    Updates

---
+++ precache-manifest.4e84598c8eee456d7f8c6cdc0b477667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbfd13ba5c05ee4ab62e7d85f079175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74dd0bdaf9209423d83d",
+    "url": "/static/css/2.50ee3336.chunk.css"
+  },
+  {
+    "revision": "2b37eb10f039190b0cd3",
+    "url": "/static/css/main.b4a29635.chunk.css"
+  },
+  {
+    "revision": "74dd0bdaf9209423d83d",
+    "url": "/static/js/2.83e5fed8.chunk.js"
+  },
+  {
+    "revision": "2b37eb10f039190b0cd3",
+    "url": "/static/js/main.b7ab3891.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27b7eeac024f151536b5bb42e50f5407",
+    "url": "/static/media/crosshairs-gps.27b7eeac.svg"
+  }
+]);
